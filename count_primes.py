994745b37b34dc45cb3704a2eb80f7a7ba0b4cad
JoshuaPark823 <joshuapark823@gmail.com>
2020-05-19 21:28:38 -0400

    Latest updates. Removed sieve()

--- count_primes.py
+++ count_primes.py
@@ -8,38 +8,39 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
 
-        # Plan: We will be implementing the Algorithm for Sieve of Eratosthenes
+        # Plan: We will be implementing the Algorithm for Sieve of Eratosthenes (With our variation)
         # Initialize an array of odd numbers from 2->N (have 2 initially inside the array)
         # Do a little cleaning while initializing so that numbers divisible by 3,4,5, and 7 aren't added.
-        array_odd = [2]
+        array_primes = [2,3,5,7]
 
         # For all the odd integers from 1 -> n, if they don't divide 3, 4, 5, and 7, add them into the array.
-        for number in range(3, n, 2):
+        for number in range(11, n, 2):
             
             if number % 3 == 0 or number % 4 == 0 or number % 5 == 0 or number % 7 == 0:
                 continue
 
-            array_odd.append(number)
+            array_primes.append(number)
 
-        # Call ur Sieve function with the filtered array of numbers and index = 0
-        return len(self.sieve(array_odd, 1))
+        num_primes = len(array_primes)
+        print(num_primes)
 
-    def sieve(self, array, index: int):
+        # Loop through the array of perceived primes and filter all non-prime numbers out
+        for d_index in range(len(array_primes)):
+            for n_index in range(d_index + 1, len(array_primes)):
 
-        if index > len(array):
-            return array
+                # For each number that divides the number at the current index, decrease the number of primes by 1
+                if array_primes[n_index] % array_primes[d_index] == 0:
+                    num_primes -= 1
 
-        else:
-            for num in range(index, len(array)-1):
-                if array[num] % array[index] == 0:
-                    print(array[num])
-                    print(array[index])
-                    array.pop(num)
+            print(array_primes)
 
-            print(array)
-            return self.sieve(array, index+1)
+        print(num_primes)
+        return num_primes
+
 
 if __name__ == "__main__":
 
     test = Solution()
-    test.countPrimes(100)
+    test.countPrimes(200)
+
+    
