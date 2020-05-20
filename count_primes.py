40e69ef0bed7989be70ba3933e00307b0a250089
JoshuaPark823 <joshuapark823@gmail.com>
2020-05-20 10:39:53 -0400

    Unfinished, found the bug, was a duplicate count

--- count_primes.py
+++ count_primes.py
@@ -12,6 +12,7 @@ class Solution:
         # Initialize an array of odd numbers from 2->N (have 2 initially inside the array)
         # Do a little cleaning while initializing so that numbers divisible by 3,4,5, and 7 aren't added.
         array_primes = [2,3,5,7]
+        duplicates = {}
 
         # For all the odd integers from 1 -> n, if they don't divide 3, 4, 5, and 7, add them into the array.
         for number in range(11, n, 2):
@@ -26,11 +27,20 @@ class Solution:
 
         # Loop through the array of perceived primes and filter all non-prime numbers out
         for d_index in range(len(array_primes)):
-            for n_index in range(d_index + 1, len(array_primes)):
 
-                # For each number that divides the number at the current index, decrease the number of primes by 1
-                if array_primes[n_index] % array_primes[d_index] == 0:
-                    num_primes -= 1
+            if d_index + 1 < len(array_primes):
+                for n_index in range(d_index + 1, len(array_primes)):
+
+                    # For each number that divides the number at the current index, decrease the number of primes by 1
+                    if array_primes[n_index] % array_primes[d_index] == 0:
+                        print("FOUND ONE")
+                        print(array_primes[n_index])
+                        print(array_primes[d_index])
+
+                        # Remove it from the array
+                        duplicates.append(array_primes.pop(n_index))
+                        num_primes -= 1
+                        print(num_primes)
 
             print(array_primes)
 
