c5cfad7779f7dff434b9dc48d20e1563bf507a43
JoshuaPark823 <joshuapark823@gmail.com>
2020-05-29 18:34:03 -0400

    Completed Sieve of Eratosthenes. Not super fast but within constraints.

--- count_primes.py
+++ count_primes.py
@@ -8,43 +8,34 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
 
-        # Some edge case checks. The modifications to the loop only modify it by a constant so the change to the 
-        # complexity of the first loop on line 28 is still O(n). But conditionals also run in constant time so w/e.
         if n <= 2:
             return 0
+    
+        hash_map = {}
+        num_primes = 0
 
-        elif n <= 5:
-            if n == 3:
-                return 1
-            return 2
+        for i in range (n):
+            hash_map[i] = True
+        
+        p = 2
+        
+        while (p * p <= n):
+            if (hash_map[p] is True):
+                for i in range (p * 2, n, p):
+                    hash_map[i] = False
 
-        elif n <= 7:
-            return 3
+            p += 1
 
-        # Initialize array_primes with a few early prime numbers.
-        array_primes = [2,3,5,7]
+        hash_map[0] = False
+        hash_map[1] = False
 
-        # For all the odd integers from 1 -> n, if they don't divide 3, 4, 5, and 7, add them into the array.
-        for number in range(11, n, 2):
-            
-            if number % 3 == 0 or number % 4 == 0 or number % 5 == 0 or number % 7 == 0:
-                continue
-
-            array_primes.append(number)
-
-        num_primes = len(array_primes)
-
-        # Loop through the array of perceived primes and filter all non-prime numbers out
-        for d_index in range(len(array_primes)):
-            for n_index in range(d_index + 1, len(array_primes)):
-
-                # To avoid double-counting of a large number with both factors in the same set, set it as
-                # negative after counting for it, then add a sign check in the conditional.
-                if array_primes[n_index] > 0 and array_primes[n_index] % array_primes[d_index] == 0:
-                    num_primes -= 1
-                    array_primes[n_index] = -array_primes[n_index]
-
+        for i in range (len(hash_map)):
+            if hash_map[i] is True:
+                num_primes += 1
+        
         return num_primes
+
+        
 
 
 if __name__ == "__main__":
