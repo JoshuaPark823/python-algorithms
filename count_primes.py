3812bb71f295aca6ab8044b46dd4bc889a678326
JoshuaPark823 <joshuapark823@gmail.com>
2020-06-03 19:36:40 -0400

    Updated whitespace for complete SoE algo

--- count_primes.py
+++ count_primes.py
@@ -1,9 +1,11 @@
-# Count the number of prime numbers less than a non-negative number, n.
+"""
+Count the number of prime numbers less than a non-negative number, n.
 
-# Example:
-#     Input: 10
-#     Output: 4
-#     Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+Example:
+    Input: 10
+    Output: 4
+    Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+"""
 
 class Solution:
     def countPrimes(self, n: int) -> int:
@@ -41,13 +43,8 @@ class Solution:
         
         return num_primes
 
-        
-
-
 if __name__ == "__main__":
 
     test = Solution()
     print(test.countPrimes(100000))
 
-    # temp = {2:True, 3:True, 5:True, 7:True}
-    # print(len(temp.keys()))
